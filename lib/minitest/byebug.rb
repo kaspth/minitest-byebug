87ce39605a373de5fc7ed5395735abfbe5095a5f
Kasper Timm Hansen <kaspth@gmail.com>
2015-01-20 23:53:11 +0100

    Add meager documentation.

--- lib/minitest/byebug.rb
+++ lib/minitest/byebug.rb
@@ -2,13 +2,14 @@ require 'byebug'
 require 'minitest'
 require 'minitest/test'
 
-# Start a byebug session when an assertion fails
 module Minitest
   module Byebug
+    # Set ENV['NO_SKIP'] to prevent tests from being skipped.
     def skip(msg = nil, bt = caller)
       super unless ENV['NO_SKIP']
     end
 
+    # Overrides #assert to start a byebug session on failed assertions.
     def assert(test, msg = nil)
       super
     rescue Minitest::Assertion => error
