305262c01bafe7ddcd79f055e2887cc2f2e054ef
Kasper Timm Hansen <kaspth@gmail.com>
2015-01-20 23:50:14 +0100

    Formatting fixes.

--- lib/minitest/byebug.rb
+++ lib/minitest/byebug.rb
@@ -3,22 +3,20 @@ require 'minitest'
 require 'minitest/test'
 
 # Start a byebug session when an assertion fails
-module Minitest::Byebug
-  def skip(msg = nil, bt = caller)
-    super unless ENV['NO_SKIP']
-  end
+module Minitest
+  module Byebug
+    def skip(msg = nil, bt = caller)
+      super unless ENV['NO_SKIP']
+    end
 
-  def assert(test, msg = nil)
-    begin
+    def assert(test, msg = nil)
       super
-    rescue Minitest::Assertion => e
+    rescue Minitest::Assertion => error
       warn "Assertion Failed. Dropping into byebug now:"
       byebug
-      raise e
+      raise error
     end
   end
-end
 
-class Minitest::Test
-  include Minitest::Byebug
+  Minitest::Test.send(:include, Minitest::Byebug)
 end
